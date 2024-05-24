8652347ad8bfd83b4eff4dc331e2ae731b0e0cf0
Abijeeth Vasra <abijeeth.vasra@bounteous.com>
2024-05-24 18:08:28 +0530

    hero temporary

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,12 +1,14 @@
+import hero from '../assets/hero.jpg';
 
 const Hero = () => {
-
-
-
   return (
-    <div className="bg-stone-800 mt-2 ml-3 h-60 w-1/3">
-      <p className="text-white">Hero Carousel</p>
+    <div className="max-w-[1400px] h-[300px] w-[1200px] m-auto p-4 bg-neutral-800 ml-3 relative">
+    <div
+      style={{ backgroundImage: `url(${hero})` }}
+      className='w-full h-full rounded-2xl bg-center bg-cover duration-500'>
+        <p className='flex justify-center text-white h-[200px] items-center'>Hero Carousel</p>
     </div>
+  </div>
   )
 }
 
