c0ad6e5ed0f3c0e13b5d6a44402fcf7daa38ca95
Abijeeth Vasra <abijeeth.vasra@bounteous.com>
2024-05-24 15:29:36 +0530

    navbar icons

--- src/Components/NavBarLinks.tsx
+++ src/Components/NavBarLinks.tsx
@@ -1,4 +1,7 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faFilm, faTv, faSearch } from '@fortawesome/free-solid-svg-icons';
+
 
 type PropsType = {
     url: string;
@@ -6,14 +9,33 @@ type PropsType = {
 }
 
 const NavBarLinks = ({url,name}:PropsType) => {
+    const getIcon = (url) => {
+        switch (url) {
+          case '/':
+            return faHome;
+          case '/moviesplp':
+            return faFilm;
+          case '/showsplp':
+            return faTv;
+          case '/search':
+            return faSearch;
+          default:
+            return null;
+        }
+      };
   return (
     <div>
-        <Link 
-            to={url}  
-            className={location.pathname === `${url}` ? 'text-yellow-500' : 'text-white'}
+        <div className="mb-10">
+            <Link 
+                to={url}  
+                className={location.pathname === url ? 'text-red-500' : 'text-white'}
             >
-                {name}
-        </Link>
+                <div className="flex flex-col items-center justify-center"> 
+                    {getIcon(url) && <FontAwesomeIcon icon={getIcon(url)} className="mr-2" />}
+                    {name}
+                </div>
+            </Link>
+        </div>
     </div>
   )
 }
